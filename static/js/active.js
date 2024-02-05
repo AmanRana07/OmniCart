6b61db237f24690359813e0777fe14db19c7ba5e
DEBOLINA <debolinaoli10@gmail.com>
2024-02-05 09:23:06 +0530

    contactus page

--- static/js/active.js
+++ static/js/active.js
@@ -390,6 +390,14 @@ Version:1.0
 	 
 	
 
+	const toggles = document.querySelectorAll('.faq-toggle')
+
+toggles.forEach(toggle => {
+    toggle.addEventListener('click', () => {
+        toggle.parentNode.classList.toggle('active')
+    })
+})
+
 })(jQuery);
 
 
